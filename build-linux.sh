f25ebb620aa03c2fe46dbf68ad48f61575b84373
Walker de Alencar <walkeralencar@gmail.com>
2025-06-11 15:04:21 -0300

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -35,120 +35,120 @@ echo "Will use $job thread(s)"
 # Vcash home dir
 echo "Creating ~/pila/ dir"
 mkdir -p ~/pila/
-VCASH_ROOT=$HOME/pila/
+PILA_ROOT=$HOME/pila/
 
 # Remove build.log file
-rm -f $VCASH_ROOT/build.log
+rm -f $PILA_ROOT/build.log
 
 # Backup dir
-mkdir -p $VCASH_ROOT/backup/
+mkdir -p $PILA_ROOT/backup/
 
 # Check src dir & backup deps
 ALL_DEPS=0
-if [[ -d "$VCASH_ROOT/src" ]]; then
-	if [[ -d "$VCASH_ROOT/src/deps/boost" && "$VCASH_ROOT/src/deps/db" && "$VCASH_ROOT/src/deps/openssl" ]]; then
-		mv -f $VCASH_ROOT/src/deps/ $VCASH_ROOT/backup/
-		echo "Deps backed up." | tee -a $VCASH_ROOT/build.log
+if [[ -d "$PILA_ROOT/src" ]]; then
+	if [[ -d "$PILA_ROOT/src/deps/boost" && "$PILA_ROOT/src/deps/db" && "$PILA_ROOT/src/deps/openssl" ]]; then
+		mv -f $PILA_ROOT/src/deps/ $PILA_ROOT/backup/
+		echo "Deps backed up." | tee -a $PILA_ROOT/build.log
 		ALL_DEPS=1
-	elif [[ -d "$VCASH_ROOT/backup/deps/boost" && "$VCASH_ROOT/backup/deps/db" && "$VCASH_ROOT/backup/deps/openssl" ]]; then
-		echo "Deps already backed up." | tee -a $VCASH_ROOT/build.log
+	elif [[ -d "$PILA_ROOT/backup/deps/boost" && "$PILA_ROOT/backup/deps/db" && "$PILA_ROOT/backup/deps/openssl" ]]; then
+		echo "Deps already backed up." | tee -a $PILA_ROOT/build.log
 		ALL_DEPS=1
 	fi
 else
-	if [[ -d "$VCASH_ROOT/backup/deps/boost" && "$VCASH_ROOT/backup/deps/db" && "$VCASH_ROOT/backup/deps/openssl" ]]; then
-		echo "Deps already backed up." | tee -a $VCASH_ROOT/build.log
+	if [[ -d "$PILA_ROOT/backup/deps/boost" && "$PILA_ROOT/backup/deps/db" && "$PILA_ROOT/backup/deps/openssl" ]]; then
+		echo "Deps already backed up." | tee -a $PILA_ROOT/build.log
 		ALL_DEPS=1
 	fi
 fi
 
 # Remove src dir
-echo "Clean before clone" | tee -a $VCASH_ROOT/build.log
-rm -Rf $VCASH_ROOT/src/
+echo "Clean before clone" | tee -a $PILA_ROOT/build.log
+rm -Rf $PILA_ROOT/src/
 
 # Check existing pila binary
-echo "Check existing binary" | tee -a $VCASH_ROOT/build.log
-if [[ -f "$VCASH_ROOT/pilad" ]]; then
-	BACKUP_VCASHD="pilad-$(date +%s)"
-	echo "Existing pilad binary ! Let's backup." | tee -a $VCASH_ROOT/build.log
-	mkdir -p $VCASH_ROOT/backup/
-	mv $VCASH_ROOT/pilad $VCASH_ROOT/backup/$BACKUP_VCASHD
+echo "Check existing binary" | tee -a $PILA_ROOT/build.log
+if [[ -f "$PILA_ROOT/pilad" ]]; then
+	BACKUP_PILAD="pilad-$(date +%s)"
+	echo "Existing pilad binary ! Let's backup." | tee -a $PILA_ROOT/build.log
+	mkdir -p $PILA_ROOT/backup/
+	mv $PILA_ROOT/pilad $PILA_ROOT/backup/$BACKUP_PILAD
 	rm -f pilad
 fi
 
 # Github
-echo "Git clone pila in src dir" | tee -a $VCASH_ROOT/build.log
-cd $VCASH_ROOT/
+echo "Git clone pila in src dir" | tee -a $PILA_ROOT/build.log
+cd $PILA_ROOT/
 git clone https://github.com/openpila/pila.git src
 
 # OpenSSL
 function build_openssl {
-	echo "OpenSSL Install" | tee -a $VCASH_ROOT/build.log
-	cd $VCASH_ROOT
-	rm -Rf $VCASH_ROOT/src/deps/openssl/
+	echo "OpenSSL Install" | tee -a $PILA_ROOT/build.log
+	cd $PILA_ROOT
+	rm -Rf $PILA_ROOT/src/deps/openssl/
 	wget $OPENSSL_URL
 	echo "$OPENSSL_SHA  openssl-$OPENSSL_VER.tar.gz" | sha256sum -c
 	tar -xzf openssl-$OPENSSL_VER.tar.gz
 	cd openssl-$OPENSSL_VER
-	mkdir -p $VCASH_ROOT/src/deps/openssl/
-	./config threads no-comp --prefix=$VCASH_ROOT/src/deps/openssl/
-	make -j$job depend && make -j$job && make install && touch $VCASH_ROOT/src/deps/openssl/current_openssl_$OPENSSL_VER
+	mkdir -p $PILA_ROOT/src/deps/openssl/
+	./config threads no-comp --prefix=$PILA_ROOT/src/deps/openssl/
+	make -j$job depend && make -j$job && make install && touch $PILA_ROOT/src/deps/openssl/current_openssl_$OPENSSL_VER
 	# Clean
-	cd $VCASH_ROOT
-	echo "Clean after OpenSSL install" | tee -a $VCASH_ROOT/build.log
+	cd $PILA_ROOT
+	echo "Clean after OpenSSL install" | tee -a $PILA_ROOT/build.log
 	rm -Rf openssl-$OPENSSL_VER/
 	rm openssl-$OPENSSL_VER.tar.gz
 }
 
 # DB
 function build_db {
-	cd $VCASH_ROOT
-	rm -Rf $VCASH_ROOT/src/deps/db/
+	cd $PILA_ROOT
+	rm -Rf $PILA_ROOT/src/deps/db/
 	wget --no-check-certificate $DB_URL
 	echo "$DB_SHA  db-$DB_VER.tar.gz" | sha256sum -c
 	tar -xzf db-*.tar.gz
-	echo "Compile & install Berkeley DB in deps folder" | tee -a $VCASH_ROOT/build.log
+	echo "Compile & install Berkeley DB in deps folder" | tee -a $PILA_ROOT/build.log
 	cd db-$DB_VER/build_unix/
-	mkdir -p $VCASH_ROOT/src/deps/db/
-	../dist/configure --enable-cxx --disable-shared --prefix=$VCASH_ROOT/src/deps/db/
-	make -j$job && make install && touch $VCASH_ROOT/src/deps/db/current_db_$DB_VER
+	mkdir -p $PILA_ROOT/src/deps/db/
+	../dist/configure --enable-cxx --disable-shared --prefix=$PILA_ROOT/src/deps/db/
+	make -j$job && make install && touch $PILA_ROOT/src/deps/db/current_db_$DB_VER
 	# Clean
-	cd $VCASH_ROOT
-	echo "Clean after Berkeley DB install" | tee -a $VCASH_ROOT/build.log
+	cd $PILA_ROOT
+	echo "Clean after Berkeley DB install" | tee -a $PILA_ROOT/build.log
 	rm -Rf db-$DB_VER/
 	rm db-$DB_VER.tar.gz
 }
 
 # Boost
 function build_boost {
-	cd $VCASH_ROOT
-	rm -Rf $VCASH_ROOT/src/deps/boost/
+	cd $PILA_ROOT
+	rm -Rf $PILA_ROOT/src/deps/boost/
 	wget $BOOST_URL
 	echo "$BOOST_SHA  boost_$BOOST_VER.tar.gz" | sha256sum -c
-	echo "Extract boost" | tee -a $VCASH_ROOT/build.log
+	echo "Extract boost" | tee -a $PILA_ROOT/build.log
 	tar -xzf boost_$BOOST_VER.tar.gz
-	echo "mv boost to deps folder & rename" | tee -a $VCASH_ROOT/build.log
+	echo "mv boost to deps folder & rename" | tee -a $PILA_ROOT/build.log
 	mv boost_$BOOST_VER src/deps/boost
-	cd $VCASH_ROOT/src/deps/boost/
-	echo "Build boost system" | tee -a $VCASH_ROOT/build.log
+	cd $PILA_ROOT/src/deps/boost/
+	echo "Build boost system" | tee -a $PILA_ROOT/build.log
 	./bootstrap.sh
 	./bjam -j$job link=static toolset=gcc cxxflags=-std=gnu++0x --with-system release &
-	touch $VCASH_ROOT/src/deps/boost/current_boost_$BOOST_VER
+	touch $PILA_ROOT/src/deps/boost/current_boost_$BOOST_VER
 	# Clean
-	cd $VCASH_ROOT
-	echo "Clean after Boost install" | tee -a $VCASH_ROOT/build.log
+	cd $PILA_ROOT
+	echo "Clean after Boost install" | tee -a $PILA_ROOT/build.log
 	rm boost_$BOOST_VER.tar.gz
 }
 
 if [[ $ALL_DEPS == 1 ]]; then
-	mv $VCASH_ROOT/backup/deps/boost/ $VCASH_ROOT/src/deps/
+	mv $PILA_ROOT/backup/deps/boost/ $PILA_ROOT/src/deps/
 	# Temp
-	if ! [[ -f "$VCASH_ROOT/src/deps/boost/current_boost_$BOOST_VER" ]]; then
-		touch $VCASH_ROOT/src/deps/boost/current_boost_$BOOST_VER
+	if ! [[ -f "$PILA_ROOT/src/deps/boost/current_boost_$BOOST_VER" ]]; then
+		touch $PILA_ROOT/src/deps/boost/current_boost_$BOOST_VER
 	fi
-	mv $VCASH_ROOT/backup/deps/db/ $VCASH_ROOT/src/deps/
-	mv $VCASH_ROOT/backup/deps/openssl/ $VCASH_ROOT/src/deps/
-	rm -Rf $VCASH_ROOT/backup/deps/
-	echo "Deps restored." | tee -a $VCASH_ROOT/build.log
+	mv $PILA_ROOT/backup/deps/db/ $PILA_ROOT/src/deps/
+	mv $PILA_ROOT/backup/deps/openssl/ $PILA_ROOT/src/deps/
+	rm -Rf $PILA_ROOT/backup/deps/
+	echo "Deps restored." | tee -a $PILA_ROOT/build.log
 else
 	build_openssl
 	build_db
@@ -156,37 +156,37 @@ else
 fi
 
 # Deps upgrade ?
-if ! [[ -f "$VCASH_ROOT/src/deps/openssl/current_openssl_$OPENSSL_VER" ]]; then
+if ! [[ -f "$PILA_ROOT/src/deps/openssl/current_openssl_$OPENSSL_VER" ]]; then
 	build_openssl
 fi
-if ! [[ -f "$VCASH_ROOT/src/deps/db/current_db_$DB_VER" ]]; then
+if ! [[ -f "$PILA_ROOT/src/deps/db/current_db_$DB_VER" ]]; then
 	build_db
 fi
-if ! [[ -f "$VCASH_ROOT/src/deps/boost/current_boost_$BOOST_VER" ]]; then
+if ! [[ -f "$PILA_ROOT/src/deps/boost/current_boost_$BOOST_VER" ]]; then
 	build_boost
 fi
 
 # Vcash daemon
-echo "pilad bjam build" | tee -a $VCASH_ROOT/build.log
-cd $VCASH_ROOT/src/coin/test/
-../../deps/boost/bjam -j$job toolset=gcc cxxflags=-std=gnu++0x hardcode-dll-paths=false release | tee -a $VCASH_ROOT/build.log
-cd $VCASH_ROOT/src/coin/test/bin/gcc-*/release/link-static/
+echo "pilad bjam build" | tee -a $PILA_ROOT/build.log
+cd $PILA_ROOT/src/coin/test/
+../../deps/boost/bjam -j$job toolset=gcc cxxflags=-std=gnu++0x hardcode-dll-paths=false release | tee -a $PILA_ROOT/build.log
+cd $PILA_ROOT/src/coin/test/bin/gcc-*/release/link-static/
 STACK_OUT=$(pwd)
 if [[ -f "$STACK_OUT/stack" ]]; then
-	echo "pilad built !" | tee -a $VCASH_ROOT/build.log
+	echo "pilad built !" | tee -a $PILA_ROOT/build.log
 	strip $STACK_OUT/stack
-	cp $STACK_OUT/stack $VCASH_ROOT/pilad
+	cp $STACK_OUT/stack $PILA_ROOT/pilad
 	# Check if pilad is running
 	RESTART=0
 	pgrep -l pilad && RESTART=1
 else
-	cd $VCASH_ROOT/src/coin/test/
+	cd $PILA_ROOT/src/coin/test/
 	echo "pilad building error..." 
 	exit
 fi
 
 # Start or restart
-cd $VCASH_ROOT
+cd $PILA_ROOT
 
 if [[ $RESTART == 1 ]]; then
 	echo -e "\n- - - - - - - - - \n"
